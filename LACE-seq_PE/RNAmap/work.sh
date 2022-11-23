9dbad506c19b384548269f95d9f079ecbbc0912a
duzc <455208619@qq.com>
2022-11-23 20:05:55 +0800

    RNAmap

--- LACE-seq_PE/RNAmap/work.sh
+++
@@ -1,4 +0,0 @@
-python3 1.generate_bins.py ../7.AS/Srsf1_AS/SE.MATS.JC.txt |sortBed >se_bins.bed
-bedtools coverage -a ./se_bins.bed -b ../../LACE-seq_mouse_SRSF1_SRSF10/3.rmdup/merge/Srsf1_merge.rmdup.sort.bam -s -f 0.5 -split -c>se_bins.cov
-
-
